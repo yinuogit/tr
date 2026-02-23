6c46201bdbe83642d745249f4ce5f3ac74b44294
lyn <liuynliu@outlook.com>
2026-02-23 14:37:40 +0800

    Add files via upload

---
+++ phase2.js/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            不强求，不将就
+        </footer>
+    );
+};
+
+export default Footer;
